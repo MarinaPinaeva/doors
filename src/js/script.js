816ecf29e075f26f24b20cc9bdf55b7f543eca55
Marina <marina.pinaeva99@icloud.com>
2022-04-08 15:42:56 +0300

    delete

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,3 @@
-/* function myMap() {
-    const mapCanvas = document.getElementById("map");
-    const mapOptions = {
-      center: new google.maps.LatLng(51.7, -0.9), zoom: 5
-    };
-    const map = new google.maps.Map(mapCanvas, mapOptions);
-  } */
-
-
-/* 	function myMap() {
-		const element = document.getElementById('map');
-		const options = {
-			zoom: 5,
-			center: {lat: 87318658669746, lng: 53.20646285153901}, 56.8732083911975, 53.20651090579342
-		};
-		const map = new google.maps.Map(element, options);
-    }; */
-
-
-/* function initMap() {
-  const position = { lat: 56.8733, lng: 53.2065};
-
-  let map = new google.maps.Map(document.getElementById("map"), {
-    center: position,
-    zoom: 17,
-  });
-
-  let marker = new google.maps.Marker({
-    map: map,
-    position: position,
-    title: 'Двери'
-  })
-} */
 
 $(document).ready(function() {
     $(window).scroll(function(){
