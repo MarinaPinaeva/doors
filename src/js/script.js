84de5fafde60c23dbe8a6693e92aec9c2e7ddcda
Marina <marina.pinaeva99@icloud.com>
2022-08-19 18:13:57 +0300

    test

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,18 @@ $(document).ready(function() {
         $("html, body").animate({scrollTop: "0px"});
         return false;
     });
+
+    $('.slider__wrapper').slick({
+        arrows: false,
+        dots: true,
+        adaptiveHeight: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    });
+
+
 });
-
-
